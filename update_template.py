f9fedbb57133ea8ca1618261b2f13675cd2cc79c
svenb <svenbioinf@gmail.com>
2023-03-01 16:22:32 +0100

    metadata files version update

--- update_template.py
+++ update_template.py
@@ -6,6 +6,7 @@ import toml
 
 
 def main():
+    files_updated = False
     args = parse_command_line()
     project_dir = args.project_dir.resolve()
     print(f"Project directory set as: {project_dir}")
@@ -21,16 +22,25 @@ def main():
     # detect if its a nf-core workflow
     external = is_external(project_dir, external)
 
-    # get metafiles if none are present
+    # initiate metadata files if none are present
     if not metadatafiles_present(project_dir, external):
         clone(project_dir, ref_repo_clone, external)
-    # else update files
+    # else update metadata files
     else:
-        files_to_update = ["CITATION.md"]  # gitignore, licence
-        [
-            update_file(f, project_dir, ref_repo_curl, ref_repo_wget)
-            for f in files_to_update
-        ]
+        files_to_update = [
+            "CITATION.md",
+            "LICENSE",
+            ".editorconfig",
+            "pyproject.toml",
+        ]  # gitignore, licence	
+        for f in files_to_update:
+            print(f"{f} checking...")
+            if f == "pyproject.toml":
+                print(f"files_updated? {files_updated}")
+                if files_updated:
+                    update_pyproject_toml(project_dir, ref_repo_wget)
+            else:
+                files_updated=files_updated or update_file(f, project_dir, ref_repo_curl, ref_repo_wget)
 
 
 def parse_command_line():
@@ -88,6 +98,7 @@ def is_external(project_dir, external):
     if not (external):
         return False
 
+
 def metadatafiles_present(project_dir, external):
     if external:
         if pathlib.Path(project_dir, "cubi").exists() and any(
@@ -132,9 +143,6 @@ def clone(project_dir, ref_repo_clone, external):  # copy all metafiles
         )
 
 
-# remove .git .gitignore
-
-
 def get_local_checksum(project_dir, f):
     command = ["git", "hash-object", project_dir.joinpath(f)]
     sha1Sum = sp.run(
@@ -160,6 +168,44 @@ def get_ref_checksum(ref_repo_curl, f, project_dir):
         cwd=project_dir,
     )
     return sha1SumRef.stdout.split('"')[11]
+
+
+def update_pyproject_toml(project_dir, ref_repo_wget):
+    f = "pyproject.toml"
+    user_response = input(f"Update metadata files version in {f}? (y/n)")
+    answers = {
+        "yes": True,
+        "y": True,
+        "yay": True,
+        "no": False,
+        "n": False,
+        "nay": False,
+    }
+    try:
+        do_update = answers[user_response]
+    except KeyError:
+        raise ValueError(
+            f"That was a yes or no question, but you answered: {user_response}"
+        )
+
+    if do_update:
+        command = [
+            "wget",
+            ref_repo_wget + f,
+            "-O" + f + ".temp",
+        ]  # -O to overwrite existing file
+        sp.call(command, cwd=project_dir)
+        version_new = toml.load(
+            pathlib.Path(project_dir, f + ".temp"), _dict=dict
+        )
+        version_old = toml.load(pathlib.Path(project_dir, f), _dict=dict)
+        version_new = toml.load(pathlib.Path(project_dir, f+".temp"), _dict=dict)
+        version_new=version_new["cubi"]["metadata"]["version"]
+        version_old["cubi"]["metadata"]["version"] = version_new
+        toml.dumps(version_old, encoder=None)
+        with open(pathlib.Path(project_dir, f), "w") as text_file:
+            text_file.write(toml.dumps(version_old, encoder=None))
+        print(f"{f} updated!")
 
 
 def update_file(f, project_dir, ref_repo_curl, ref_repo_wget):
@@ -192,11 +238,14 @@ def update_file(f, project_dir, ref_repo_curl, ref_repo_wget):
                 "-O" + f,
             ]  # -O to overwrite existing file
             sp.call(command, cwd=project_dir)
-            print("Updated!")
+            print(f"{f} updated!")
+            return True
         else:
-            print("Nothing to update.")
+            return False
+            print(f"{f} nothing to update.")
     else:
-        print("Nothing to update.")
+        return False
+        print("f{f} nothing to update.")
 
 
 def report_script_version():
