969938778cc17dec6b0bd7521ab426090bfa2bf4
Sven Willger <sven.willger@gmail.com>
2024-01-18 10:20:17 +0100

    Issues #39 and #40 have been addressed

--- cubi-tools/update_metadata.py
+++ cubi-tools/update_metadata.py
@@ -28,8 +28,11 @@ def main():
     if args.version:
         print(f"\nScript version: {report_script_version()}")
 
-    project_dir = args.project_dir.resolve()
-    print(f"\nProject directory set as: {project_dir}")
+    # check if project directory exist:
+    project_dir = pathlib.Path(args.project_dir).resolve()
+    assert project_dir.is_dir(), f"The project directory {project_dir} doesn't exist!"
+    print(f"Project directory set as: {project_dir}\n")
+
     ref_repo = args.ref_repo
     source = args.source
     external = args.external
@@ -37,8 +40,8 @@ def main():
 
     # Since using the online github repo directly to update the local metadata files
     # is resulting in hitting an API rate limit fairly quickly a local copy is needed.
-    # The location of the template_metadata folder holding branch/version tag of interest
-    # is on the same level as the project directory
+    # The location of the template_metadata folder holding branch/version tag
+    # of interest is on the same level as the project directory
     metadata_dir = pathlib.Path(
         pathlib.Path(f"{project_dir}").resolve().parents[0],
         f"update_metadata_temp/{source}",
@@ -49,21 +52,19 @@ def main():
         workflow_dir = external_repo(project_dir, external, dryrun)
         print(f"\nExternal set as: {external}\n")
         print(
-            f"Metadata files will be updated in: {external_repo(project_dir, external, dryrun)}\n"
+            "Metadata files will be updated in: "
+            f"{external_repo(project_dir, external, dryrun)}\n"
         )
     else:
         workflow_dir = external_repo(project_dir, external, dryrun)
         print(
-            f"\nMetadata files will be updated in: {external_repo(project_dir, external, dryrun)}\n"
+            "\nMetadata files will be updated in: "
+            f"{external_repo(project_dir, external, dryrun)}\n"
         )
 
-    # Clone the 'template-metadata-files' branch or version tag into a local folder if it exists
-    try:
-        clone(project_dir, ref_repo, source, metadata_dir, dryrun)
-    except AssertionError:
-        raise AssertionError(
-            f"The repository you entered or the branch or version tag named '{source}' doesn't exist"
-        ) from None
+    # Clone the 'template-metadata-files' branch or version tag
+    # into a local folder if it exists
+    clone(project_dir, ref_repo, source, metadata_dir, dryrun)
 
     # files that will be updated
     files_to_update = [
@@ -78,19 +79,23 @@ def main():
         if f == "pyproject.toml":
             update_pyproject_toml(workflow_dir, metadata_dir, source, dryrun)
         else:
-            print(f"Comparing if local '{f}' differs from version in branch/version tag "
-              f"'{source}' in the 'template-metadata-files' repo")
+            print(
+                f"Comparing if local '{f}' differs from version in branch/version tag "
+                f"'{source}' in the 'template-metadata-files' repo"
+            )
             update_file(f, workflow_dir, metadata_dir, dryrun)
 
     # detect if metafiles temp folder should be kept
     if keep:
         print(
-            f"\nYou want to keep the files of the branch/version tag '{source}' of the 'template-metadata-files' folder.\n"
+            f"\nYou want to keep the files of the branch/version tag '{source}' "
+            "of the 'template-metadata-files' folder.\n"
             f"It's located at '{metadata_dir}'"
         )
     else:
         rm_temp(metadata_dir.parent, dryrun)
-        #print("\nThe 'update_metadata_temp' folder with all files and subfolders has been deleted!")
+        # print("\nThe 'update_metadata_temp' folder with all files
+        # and subfolders has been deleted!")
 
     print("\nUPDATE COMPLETED!")
 
@@ -141,8 +146,8 @@ def parse_command_line():
         action="store_true",
         default=False,
         dest="keep",
-        help="If False (default), the metadata files source repo will be deleted when script finishes,"
-        "else it will be kept.",
+        help="If False (default), the metadata files source repo "
+        "will be deleted when script finishes else it will be kept.",
     )
     parser.add_argument(
         "--dry-run",
@@ -173,19 +178,21 @@ def clone(workflow_dir, ref_repo, source, metadata_dir, dryrun):
     Check if the branch or version tag that contains the desired metadata files
     is already in a temp folder parallel to the project directory.
     If that is not the case, the desired branch or version tag will be cloned into
-    a temp folder parallel to the project directory unless the branch or version tag don't exist,
-    then an AssertionError will be called to stop the script.
-    If a temp folder for the branch or version tag exists this folder is getting updated via
-    a git pull command.
+    a temp folder parallel to the project directory unless the branch or version tag
+    don't exist, then an AssertionError will be called to stop the script.
+    If a temp folder for the branch or version tag exists this folder is getting
+    updated via a git pull command.
     """
     if dryrun:
         if not metadata_dir.is_dir():
             print(
-                f"The requested branch/version tag (default: main) is being copied to {metadata_dir}.\n"
+                "The requested branch/version tag (default: main) is being "
+                f"copied to {metadata_dir}.\n"
             )
         else:
             print(
-                "The requested branch/version tag (default: main) already exists and is getting updated.\n"
+                "The requested branch/version tag (default: main) already "
+                "exists and is getting updated.\n"
             )
     else:
         if not metadata_dir.is_dir():
@@ -210,7 +217,10 @@ def clone(workflow_dir, ref_repo, source, metadata_dir, dryrun):
             # Git will throw a error message if you try to clone a repo/branch/tag
             # that doesn't exist that contains the string 'fatal:'
             warning = "fatal:"
-            assert warning not in str(clone_cmd.stderr.strip())
+            assert warning not in str(clone_cmd.stderr.strip()), (
+                "The repository you entered or the branch or version tag "
+                f"named '{source}' doesn't exist"
+            )
         else:
             command = [
                 "git",
@@ -229,7 +239,8 @@ def clone(workflow_dir, ref_repo, source, metadata_dir, dryrun):
 
 def get_local_checksum(workflow_dir, f):
     """
-    The MD5 checksum for all metadata files in the local project directory is determined.
+    The MD5 checksum for all metadata files in the
+    local project directory is determined.
     """
     if workflow_dir.joinpath(f).is_file():
         with open(workflow_dir.joinpath(f), "rb") as local_file:
@@ -244,7 +255,8 @@ def get_local_checksum(workflow_dir, f):
 
 def get_ref_checksum(metadata_dir, f):
     """
-    The MD5 checksum for all metadata files in the temp folder for the desired branch or version tag is determined.
+    The MD5 checksum for all metadata files in the temp folder
+    for the desired branch or version tag is determined.
     """
     with open(metadata_dir.joinpath(f), "rb") as ref_file:
         # read contents of the file
@@ -256,12 +268,17 @@ def get_ref_checksum(metadata_dir, f):
 
 def update_file(f, workflow_dir, metadata_dir, dryrun):
     """
-    The MD5 checksum of the the local metadata file(s) and the metadata file(s) in the desired
-    branch or version tag are being compared. If they differ a question to update for each different
+    The MD5 checksum of the the local metadata file(s) and the metadata
+    file(s) in the desired branch or version tag are being compared.
+    If they differ a question to update for each different
     metadata file pops up. If an update is requested it will be performed.
     """
     if dryrun:
-        print(f"Dry run! {f} updated!")
+        print(f"The versions of '{f}' differ!")
+        print("Local MD5 checksum: (some MD5 checksum)")
+        print("Remote MD5 checksum: (some other MD5 checksum)")
+        print(f"Update '{f}'(y/n)? y")
+        print(f"Dry run! '{f}' would be updated!")
     else:
         local_sum = get_local_checksum(workflow_dir, f)
         ref_sum = get_ref_checksum(metadata_dir, f)
@@ -277,11 +294,7 @@ def update_file(f, workflow_dir, metadata_dir, dryrun):
                     metadata_dir.joinpath(f),
                     workflow_dir.joinpath(f),
                 ]
-                sp.run(
-                    command,
-                    cwd=workflow_dir,
-                    check=False
-                )
+                sp.run(command, cwd=workflow_dir, check=False)
                 print(f"'{f}' was updated!")
             else:
                 print(f"'{f}' was NOT updated!")
@@ -292,16 +305,27 @@ def update_file(f, workflow_dir, metadata_dir, dryrun):
 
 def update_pyproject_toml(workflow_dir, metadata_dir, source, dryrun):
     """
-    The 'pyproject.toml' is treated a little bit differently. First, there is a check if
-    the file even exists in the project directory. If that is not the case it will be copied
-    into that folder from the desired branch or version tag.
-    If the file is present it will be checked if the cubi.metadata.version (and only that information!)
-    differs between the local and the requested branch or version tag version. If that is the case the
-    cubi.metadata.version is getting updated.
+    The 'pyproject.toml' is treated a little bit differently. First, there is
+    a check if the file even exists in the project directory. If that is not the
+    case it will be copied into that folder from the desired branch or version tag.
+    If the file is present it will be checked if the cubi.metadata.version
+    (and only that information!) differs between the local and the requested branch
+    or version tag version. If that is the case the cubi.metadata.version
+    is getting updated.
     """
     x = "pyproject.toml"
     if dryrun:
-        print(f"Dry run! '{x}' added or updated!")
+        print(f"\nThere is no 'pyproject.toml' in your folder. Add '{x}'(y/n)? y")
+        print(f"Dry run! '{x}' would have been added!")
+        print(
+            "\nYou updated your local repo with the 'template-metadata-files' "
+            f"in branch/version tag '{source}'."
+            f"\nDo you want to update the metadata files version in '{x}'(y/n)? y"
+        )
+        print(
+            f"Dry run! Metadata version in '{x}' would have been updated from version "
+            "'v1' to version 'v2'!"
+        )
     else:
         if not workflow_dir.joinpath(x).is_file():
             question = user_response(
@@ -314,11 +338,7 @@ def update_pyproject_toml(workflow_dir, metadata_dir, source, dryrun):
                     metadata_dir.joinpath(x),
                     workflow_dir.joinpath(x),
                 ]
-                sp.run(
-                    command,
-                    cwd=workflow_dir,
-                    check=False
-                )
+                sp.run(command, cwd=workflow_dir, check=False)
                 print(f"'{x}' was added!")
             else:
                 print(f"'{x}' was NOT added!")
@@ -329,11 +349,7 @@ def update_pyproject_toml(workflow_dir, metadata_dir, source, dryrun):
                 metadata_dir.joinpath(x),
                 pathlib.Path(workflow_dir, x + ".temp"),
             ]
-            sp.run(
-                command,
-                cwd=workflow_dir,
-                check=False
-            )
+            sp.run(command, cwd=workflow_dir, check=False)
             version_new = toml.load(pathlib.Path(workflow_dir, x + ".temp"), _dict=dict)
             version_old = toml.load(pathlib.Path(workflow_dir, x), _dict=dict)
             version_new = version_new["cubi"]["metadata"]["version"]
@@ -342,8 +358,10 @@ def update_pyproject_toml(workflow_dir, metadata_dir, source, dryrun):
 
             if version_old_print != version_new:
                 question = user_response(
-                    f"\nYou updated your local repo with the 'template-metadata-files' in branch/version tag '{source}'."
-                    f"\nDo you want to update the metadata files version in '{x}'")
+                    "\nYou updated your local repo with the 'template-metadata-files' "
+                    f"in branch/version tag '{source}'."
+                    f"\nDo you want to update the metadata files version in '{x}'"
+                )
 
                 if question:
                     toml.dumps(version_old, encoder=None)
@@ -359,7 +377,8 @@ def update_pyproject_toml(workflow_dir, metadata_dir, source, dryrun):
                 else:
                     pathlib.Path(workflow_dir, x + ".temp").unlink()
                     print(
-                        f"'{x}' was NOT updated from version '{version_old_print}' to version '{version_new}'!"
+                        f"'{x}' was NOT updated from version '{version_old_print}' "
+                        f"to version '{version_new}'!"
                     )
             else:
                 pathlib.Path(workflow_dir, x + ".temp").unlink()
@@ -398,8 +417,8 @@ def user_response(question):
 
 def external_repo(project_dir, external, dryrun):
     """
-    Function to create a cubi folder where the CUBI metadata files will be copied/updated
-    if the user stated that the project is from external.
+    Function to create a cubi folder where the CUBI metadata files will be
+    copied/updated if the user stated that the project is from external.
     """
     if dryrun:
         if external:
